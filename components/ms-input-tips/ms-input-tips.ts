ce412df1f2ae02f784a8964d56d07e6b79fc841a
郑文龙 <1426034446@qq.com>
2018-01-16 10:18:24 +0800

    + 添加ms-input-tips组件（继承ms-input所有功能，并添加多了带×清除的功能，并做了×验证错误变化）

---
+++ components/ms-input-tips/ms-input-tips.ts
@@ -0,0 +1,45 @@
+import * as avalon from 'avalon2';
+import * as $ from 'jquery';
+import 'bootstrap';
+import controlComponent from '../ms-form/ms-control';
+import { emitToFormItem } from '../ms-form/utils';
+import { findParentComponent } from '../../ane-util';
+
+controlComponent.extend({
+    displayName: 'ms-input-tips',
+    template: require('./ms-input-tips.html'),
+    defaults: {
+        text: '',
+        type: 'input', 
+        disabled: false,
+        error_color:'#d2403d',
+        success_color:'#536C80',
+        tips_clear:true,
+        tips_error:false,
+        mapValueToText(value) {
+            this.text = value;
+        },
+        handleClear(){
+            this.mapValueToText('');
+            this.handleChange({
+                target: { value: '' },
+                denyValidate: true,
+                type: 'changed'
+            });
+            $(".ane-input-tips input[name="+this.col +"]").focus();
+            return false;
+        },
+        onInit: function (event) {
+            emitToFormItem(this);
+            this.$watch('value', v => {
+                this.mapValueToText(v);
+                this.handleChange({
+                    target: { value: v },
+                    denyValidate: true,
+                    type: 'changed'
+                });
+            });
+            this.mapValueToText(this.value);
+        }
+    }
+});
